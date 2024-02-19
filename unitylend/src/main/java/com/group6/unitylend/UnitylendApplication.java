ab05c016690692d1caf7e2c604273340673c0540
SURABHI2003 <91086564+SURABHI2003@users.noreply.github.com>
2024-02-19 17:17:32 +0530

    Add project

---
+++ unitylend/src/main/java/com/group6/unitylend/UnitylendApplication.java
@@ -0,0 +1,13 @@
+package com.group6.unitylend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitylendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitylendApplication.class, args);
+	}
+
+}
